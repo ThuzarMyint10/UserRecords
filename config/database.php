9a64c8b5bd2a87520c5e2fa841dfdc3242079067
ThuzarMyint10 <thuzarmyint795@gmail.com>
2024-07-05 14:58:17 +0630

    Update Redis configuration for Heroku

--- config/database.php
+++ config/database.php
@@ -142,19 +142,17 @@ return [
     'redis' => [
 
         'client' => env('REDIS_CLIENT', 'predis'),
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => parse_url(env('REDIS_URL'), PHP_URL_HOST),
+            'password' => parse_url(env('REDIS_URL'), PHP_URL_PASS),
+            'port' => parse_url(env('REDIS_URL'), PHP_URL_PORT),
+            'database' => 0, // Redis database number
+        ],
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
-        ],
-
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
